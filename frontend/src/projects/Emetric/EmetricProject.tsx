cd81d18414621023eaf9c630d66a23fdfbd9d56e
Ephraim Park <ephiepark@gmail.com>
2025-05-02 08:46:51 +0900

    Add remove button for graph

--- frontend/src/projects/Emetric/EmetricProject.tsx
+++ frontend/src/projects/Emetric/EmetricProject.tsx
@@ -10,6 +10,10 @@ const EmetricProject: React.FC = () => {
     setGraphs([...graphs, newGraphId]);
   };
 
+  const handleRemoveGraph = (graphIdToRemove: string) => {
+    setGraphs(graphs.filter(graphId => graphId !== graphIdToRemove));
+  };
+
   return (
     <div className="emetric-project-container">
       <div className="emetric-header">
@@ -24,7 +28,20 @@ const EmetricProject: React.FC = () => {
       
       <div className="graphs-container">
         {graphs.map(graphId => (
-          <Graph key={graphId} id={graphId} />
+          <div key={graphId} className="graph-wrapper">
+            <div className="graph-header">
+              <button 
+                className="remove-graph-button" 
+                onClick={() => handleRemoveGraph(graphId)}
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"/>
+                </svg>
+                Remove
+              </button>
+            </div>
+            <Graph id={graphId} />
+          </div>
         ))}
       </div>
     </div>
